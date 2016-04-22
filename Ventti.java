df12eaac39e762dd97f6c5d041666adc867436d3
Jerrytr <traskelin.jerry@gmail.com>
2016-04-22 13:18:30 +0300

    lisäsin kommentin

--- Ventti.java
+++ Ventti.java
@@ -6,11 +6,10 @@ public class Ventti {
     ArrayList pakka = new ArrayList();
     
     public void alustaPakka() {
-        //this.pakka=pakka;
         pakka.clear();
 
     }
-    public void taytaPakka() {
+    public void taytaPakka() { //Täyttää pakan "korteilla" 2-14
         for (int i=2; i<=14; i++) {
             for (int j=1; j<=4; j++) {
                 pakka.add(i);
