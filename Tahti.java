561761fe1ab461410b9115a4e197fb96d9796ab3
Tuomas R <Ravander@users.noreply.github.com>
2016-05-02 10:28:49 +0300

    kaikki

--- Tahti.java
+++ Tahti.java
@@ -1,13 +1,13 @@
 package rahapeliSimulaattori;
-import java.util.Random;
 
 public class Tahti extends Peli {
 
     private char[] merkit = { 'a', 'b', 'c', 'd', '*' };
     private int[] lukitut = { 0, 0, 0 };
     
-    public int[] getLukitut() {
-        return lukitut;
+    public int[] getLukitut() { return lukitut; }
+    public void setLukitut(int[] lukitut) {
+        this.lukitut = lukitut;
     }
     
     private char arvoMerkki() { 
@@ -24,16 +24,70 @@ public class Tahti extends Peli {
                 riviLista[i] = arvoMerkki();
             }
             tulosRivi += riviLista[i];
-        }
-        
+        }    
         return tulosRivi;
     }
     
-    public void lukitse(int[] lukot) {
-        
-        for ( int i = 0; i < 3; i++ ) {
-            lukitut[i] = lukot[i];
+    public void lukitse() {
+    
+        int[] lukot = new int[3];
+        System.out.println("Valitse lukitukset");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("> ");
+            lukot[i] = lukija.nextInt();
         }
+        setLukitut(lukot);
+    }
+    
+    public void resetLukot() {
+        int[] nollaus = { 0, 0, 0 };
+        setLukitut(nollaus);
+    }
+    
+    public double tarkistaVoitto(String rivi, double panos) {
+    
+        double voitto = 0;
+        switch (rivi) {
+            case "aaa":
+                voitto = panos * 3;
+                break;
+            case "bbb":
+                voitto = panos * 5;
+                break;
+            case "ccc":
+                voitto = panos * 10;
+                break;
+            case "ddd":
+                voitto = panos * 15;
+                break;
+            case "***":
+                voitto = panos * 20;
+                break;
+            default:
+                voitto -= panos;
+        }
+        return voitto;
+    }
+    
+    public double pelaa(double rahaMaara, double panos) {
+        
+        String rivi = "ooo";
+        while (rahaMaara > 0) {
+            char[] riviLista = { 'o', 'o', 'o' };
+            resetLukot();
+            rivi = arvoRivi( riviLista, getLukitut() );
+            System.out.println(rivi);
+            if (tarkistaVoitto(rivi, panos) > 0) {
+                rahaMaara += tarkistaVoitto(rivi, panos);
+            } else {
+                lukitse();
+                rivi = arvoRivi( riviLista, getLukitut() );
+                System.out.println(rivi);
+                rahaMaara += tarkistaVoitto(rivi, panos);
+            }
+            System.out.println("Sinulla on rahaa: " + rahaMaara);
+        }
+        return rahaMaara;        
     }
 }
     
